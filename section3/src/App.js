20bec7f42f6868da4d4dfca321f2cbcb4ed53823
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-05 11:14:01 +0900

    feat(childarea.jsx): 10 レンダリング最適化
    memo化、useCallbackについて

--- section3/src/App.js
+++ section3/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 import { ChildArea } from "./ChildArea";
 
@@ -8,6 +8,7 @@ export default function App() {
 
   const onChangeText = (e) => setText(e.target.value);
   const onClickOpen = () => setOpen(!open);
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
 
   return (
     <div className="App">
@@ -15,7 +16,7 @@ export default function App() {
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
