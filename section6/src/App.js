0cfcd7b88467ead78d78b4f9043ae9591634a4a8
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-08 14:54:27 +0900

    feat(section6/organisms): 20 Organismの作成

--- section6/src/App.js
+++ section6/src/App.js
@@ -2,6 +2,18 @@ import "./App.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+
+const user = {
+  name: "たえます",
+  image: "https://source.unsplash.com/S2Kwcbeaekc",
+  email: "12345@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社",
+  },
+  website: "https:google.com",
+};
 
 export default function App() {
   return (
@@ -9,6 +21,7 @@ export default function App() {
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>テスト</SecondaryButton>
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
