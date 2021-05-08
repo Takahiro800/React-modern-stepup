490f727f0cc0b7d9eb6f7a752c8b6c83a128e8c5
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-08 17:12:09 +0900

    feat(section6/page): 32 Pageの作成

--- section6/src/App.js
+++ section6/src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { DefaultLayout } from "./components/templates/DefaultLayout.jsx";
-
-const user = {
-  name: "たえます",
-  image: "https://source.unsplash.com/S2Kwcbeaekc",
-  email: "12345@example.com",
-  phone: "090-1111-2222",
-  company: {
-    name: "テスト株式会社",
-  },
-  website: "https:google.com",
-};
+import { Router } from "./router/Router";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>テスト</SecondaryButton>
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
