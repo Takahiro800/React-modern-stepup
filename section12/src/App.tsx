42fda61e2d6c04198381b9f587637a70714187f2
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-09 19:50:51 +0900

    feat(section12 src/router): 66 ルーティングの設定

--- section12/src/App.tsx
+++ section12/src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
 import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
