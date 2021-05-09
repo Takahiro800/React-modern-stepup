33f4cbd087c84864382dde70da6279d9da2abe61
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-09 10:02:05 +0900

    feat(section9): 46 返り値の型指定

--- section9/src/App.tsx
+++ section9/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Practice1 } from './practice/Practice';
+import { Practice2 } from './practice/Practice2';
 
 export default function App() {
   return (
     <div className="App">
       <Practice1 />
+      <Practice2 />
     </div>
   );
 }
