8f26ef27dad9057b7256e837db70dabae0f123fe
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-09 10:07:55 +0900

    feat(section9/src/practice/practice3.tsx): 47 変数の型指定
    n

--- section9/src/App.tsx
+++ section9/src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Practice1 } from './practice/Practice';
 import { Practice2 } from './practice/Practice2';
+import { Practice3 } from './practice/Practice3';
 
 export default function App() {
   return (
     <div className="App">
       <Practice1 />
       <Practice2 />
+      <Practice3 />
     </div>
   );
 }
