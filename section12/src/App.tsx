c0b2b68106ab8ec8168e9ceb53b3b4082b274dcf
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-09 17:53:16 +0900

    style(section12/src/theme): globalなスタイルを設定

--- section12/src/App.tsx
+++ section12/src/App.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Button, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
 export default function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">ボタン</Button>
     </ChakraProvider>
   );
