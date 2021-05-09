58977e7736c7494bd023aa111ac34652b60e4302
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-09 09:55:16 +0900

    feat(section9): 45 引数の型指定

---
+++ section9/src/typeList.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/** TypeScriptの基本の型 */
+
+
+
+// boolean
+export { }
+let bool: boolean = true;
+
+// number 数値
+let num: number = 0;
+
+// string 文字
+let str: string = "A";
+
+// Array 配列
+let arr1: Array<number> = [0, 1, 2];
