36a4e23c95560ba062213649647571e6db739cb4
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-11 21:11:56 +0900

    feat(usermanagement): 74 ユーザー一覧画面の作成

--- section12/src/components/pages/UserManagement.tsx
+++ section12/src/components/pages/UserManagement.tsx
@@ -1,5 +1,18 @@
+import { Image } from "@chakra-ui/image";
+import { Wrap, WrapItem } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです。</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="たえます"
+          fullName="Takahiro Abe"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
