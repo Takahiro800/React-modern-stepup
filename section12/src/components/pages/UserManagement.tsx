e19ef68429586b374d1dace0b8be33b10249d816
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-11 21:35:42 +0900

    feat: 75 ユーザー一覧取得機能の実装

--- section12/src/components/pages/UserManagement.tsx
+++ section12/src/components/pages/UserManagement.tsx
@@ -1,18 +1,34 @@
 import { Image } from "@chakra-ui/image";
-import { Wrap, WrapItem } from "@chakra-ui/layout";
-import { memo, VFC } from "react";
+import { Center, Wrap, WrapItem } from "@chakra-ui/layout";
+import { Spinner } from "@chakra-ui/spinner";
+import { memo, useEffect, VFC } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="たえます"
-          fullName="Takahiro Abe"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
